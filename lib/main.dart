574facf560b8e3540986935f755cae7e8c501700
Andrew Bell <fiixed@gmail.com>
2019-01-22 13:50:16 -0600

    fixed bug with image load

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,564 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+final assetImages = [
+  '\$pac',
+  'bnt',
+  'dash',
+  'exmo',
+  'ion',
+  'nav',
+  'prl',
+  'steem',
+  'wan',
+  '2give',
+  'bnty',
+  'dat',
+  'exp',
+  'iop',
+  'ncash',
+  'pungo',
+  'storj',
+  'waves',
+  'abt',
+  'booty',
+  'data',
+  'fair',
+  'iost',
+  'ndz',
+  'pura',
+  'storm',
+  'wax',
+  'act',
+  'bos',
+  'dbc',
+  'fct',
+  'iotx',
+  'nebl',
+  'qash',
+  'stq',
+  'wgr',
+  'actn',
+  'bpt',
+  'dcn',
+  'fil',
+  'iq',
+  'neo',
+  'qiwi',
+  'strat',
+  'wicc',
+  'ada',
+  'bq',
+  'dcr',
+  'fjc',
+  'itc',
+  'neos',
+  'qlc',
+  'sub',
+  'wings',
+  'add',
+  'brd',
+  'deez',
+  'fldc',
+  'jnt',
+  'neu',
+  'qrl',
+  'sumo',
+  'wpr',
+  'adx',
+  'bsd',
+  'dent',
+  'flo',
+  'jpy',
+  'nexo',
+  'qsp',
+  'sys',
+  'wtc',
+  'ae',
+  'bsv',
+  'dew',
+  'fsn',
+  'kcs',
+  'ngc',
+  'qtum',
+  'taas',
+  'x',
+  'aeon',
+  'btc',
+  'dgb',
+  'ftc',
+  'kin',
+  'nio',
+  'r',
+  'tau',
+  'xas',
+  'agi',
+  'btcd',
+  'dgd',
+  'fuel',
+  'kmd',
+  'nlc2',
+  'rads',
+  'tbx',
+  'xbc',
+  'agrs',
+  'btch',
+  'dlt',
+  'fun',
+  'knc',
+  'nlg',
+  'rap',
+  'tel',
+  'xby',
+  'aion',
+  'btcp',
+  'dnr',
+  'game',
+  'krb',
+  'nmc',
+  'rcn',
+  'ten',
+  'xcp',
+  'amb',
+  'btcz',
+  'dnt',
+  'gas',
+  'lbc',
+  'npxs',
+  'rdd',
+  'tern',
+  'xdn',
+  'amp',
+  'btdx',
+  'dock',
+  'gbp',
+  'lend',
+  'nuls',
+  'rdn',
+  'tghc',
+  'xem',
+  'ant',
+  'btg',
+  'doge',
+  'gbx',
+  'link',
+  'nxs',
+  'rep',
+  'theta',
+  'xin',
+  'apex',
+  'btm',
+  'drgn',
+  'gbyte',
+  'lkk',
+  'nxt',
+  'req',
+  'tix',
+  'xlm',
+  'appc',
+  'bts',
+  'drop',
+  'generic',
+  'loom',
+  'oax',
+  'rhoc',
+  'tkn',
+  'xmcc',
+  'ardr',
+  'btx',
+  'dta',
+  'glxt',
+  'lpt',
+  'ok',
+  'ric',
+  'tks',
+  'xmg',
+  'arg',
+  'burst',
+  'dth',
+  'gmr',
+  'lrc',
+  'omg',
+  'rise',
+  'tnb',
+  'xmo',
+  'ark',
+  'call',
+  'dtr',
+  'gno',
+  'lsk',
+  'omni',
+  'rlc',
+  'tnc',
+  'xmr',
+  'arn',
+  'cc',
+  'ebst',
+  'gnt',
+  'ltc',
+  'ong',
+  'rpx',
+  'tnt',
+  'xmy',
+  'ary',
+  'cdn',
+  'eca',
+  'gold',
+  'lun',
+  'ont',
+  'rub',
+  'tomo',
+  'xp',
+  'ast',
+  'cdt',
+  'edg',
+  'grc',
+  'maid',
+  'oot',
+  'rvn',
+  'tpay',
+  'xpa',
+  'atm',
+  'cenz',
+  'edo',
+  'grs',
+  'mana',
+  'ost',
+  'ryo',
+  'trig',
+  'xpm',
+  'audr',
+  'chain',
+  'edoge',
+  'gsc',
+  'mcap',
+  'ox',
+  'safe',
+  'trtl',
+  'xrp',
+  'auto',
+  'chat',
+  'ela',
+  'gto',
+  'mco',
+  'part',
+  'salt',
+  'trx',
+  'xsg',
+  'aywa',
+  'chips',
+  'elf',
+  'gup',
+  'mda',
+  'pasc',
+  'san',
+  'tusd',
+  'xtz',
+  'bab',
+  'cix',
+  'elix',
+  'gusd',
+  'mds',
+  'pasl',
+  'sbd',
+  'tzc',
+  'xuc',
+  'bat',
+  'clam',
+  'ella',
+  'gvt',
+  'med',
+  'pax',
+  'sberbank',
+  'ubq',
+  'xvc',
+  'bay',
+  'cloak',
+  'emc',
+  'gxlt',
+  'miota',
+  'pay',
+  'sc',
+  'unity',
+  'xvg',
+  'bcbc',
+  'cmm',
+  'emc2',
+  'gxs',
+  'mith',
+  'payx',
+  'shift',
+  'usd',
+  'xzc',
+  'bcc',
+  'cmt',
+  'eng',
+  'gzr',
+  'mkr',
+  'pgt',
+  'sib',
+  'usdc',
+  'yoyow',
+  'bcd',
+  'cnd',
+  'enj',
+  'hight',
+  'mln',
+  'pink',
+  'sky',
+  'usdt',
+  'zcl',
+  'bch',
+  'cnx',
+  'entrp',
+  'hodl',
+  'mnx',
+  'pirl',
+  'slr',
+  'utk',
+  'zec',
+  'bcn',
+  'cny',
+  'eos',
+  'hpb',
+  'mnz',
+  'pivx',
+  'sls',
+  'ven',
+  'zel',
+  'bco',
+  'cob',
+  'eql',
+  'hsr',
+  'moac',
+  'plr',
+  'smart',
+  'veri',
+  'zen',
+  'bcpt',
+  'colx',
+  'eqli',
+  'ht',
+  'mod',
+  'poa',
+  'sngls',
+  'vet',
+  'zest',
+  'bdl',
+  'coqui',
+  'equa',
+  'html',
+  'mona',
+  'poe',
+  'snm',
+  'via',
+  'zil',
+  'bela',
+  'cred',
+  'etc',
+  'huc',
+  'msr',
+  'polis',
+  'snt',
+  'vib',
+  'zilla',
+  'bix',
+  'crpt',
+  'eth',
+  'hush',
+  'mth',
+  'poly',
+  'soc',
+  'vibe',
+  'zrx',
+  'blcn',
+  'crw',
+  'ethos',
+  'icn',
+  'mtl',
+  'pot',
+  'spank',
+  'vivo',
+  'blk',
+  'cs',
+  'etn',
+  'icx',
+  'music',
+  'powr',
+  'sphtx',
+  'vrc',
+  'block',
+  'ctr',
+  'etp',
+  'ignis',
+  'mzc',
+  'ppc',
+  'srn',
+  'vrsc',
+  'blz',
+  'ctxc',
+  'eur',
+  'ink',
+  'nano',
+  'ppp',
+  'stak',
+  'vtc',
+  'bnb',
+  'cvc',
+  'evx',
+  'ins',
+  'nas',
+  'ppt',
+  'start',
+  'wabi'
+];
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var list;
+  var refreshKey = GlobalKey<RefreshIndicatorState>();
+
+  @override
+  void initState() {
+    super.initState();
+    refreshListCoin();
+  }
+
+  _getImage(String symbol) {
+    if (assetImages.contains('${symbol.toLowerCase()}')) {
+      return new Image.asset(
+        "assets/images/" + symbol.toLowerCase() + ".png",
+      );
+    } else {
+      return new Container();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      title: 'COIN TRACKER',
+      theme: ThemeData.light(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('COIN TRACER'),
+        ),
+        body: Center(
+          child: RefreshIndicator(
+              key: refreshKey,
+              child: FutureBuilder<List<CoinMarket>>(
+                future: list,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    List<CoinMarket> coins = snapshot.data;
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+                    return ListView(
+                      children: coins
+                          .map(
+                            (coin) => Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: <Widget>[
+                                    Row(
+                                      children: <Widget>[
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: <Widget>[
+                                            Container(
+                                              padding: const EdgeInsets.only(
+                                                  left: 8.0, bottom: 8.0),
+                                              child: _getImage(coin.symbol),
+                                              width: 56.0,
+                                              height: 56.0,
+                                            ),
+                                          ],
+                                        ),
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: <Widget>[
+                                            Container(
+                                              padding:
+                                                  const EdgeInsets.all(4.0),
+                                              child: Text(
+                                                  '${coin.symbol} | ${coin.name}'),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                          )
+                          .toList(),
+                    );
+                  } else if (snapshot.hasError) {
+                    Text('Error while loading Coin list ${snapshot.error}');
+                  }
+                },
+              ),
+              onRefresh: refreshListCoin),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Future<Null> refreshListCoin() {
+    refreshKey.currentState?.show(atTop: false);
+    setState(() {
+      list = fetchListCoin();
+    });
+    return null;
+  }
+}
+
+Future<List<CoinMarket>> fetchListCoin() async {
+  final api_endpoint =
+      await http.get('https://api.coinmarketcap.com/v1/ticker/');
+
+  if (api_endpoint.statusCode == 200) {
+    List coins = json.decode(api_endpoint.body);
+    return coins.map((coin) => new CoinMarket.fromJson(coin)).toList();
+  } else {
+    throw Exception('Failed to load Coin list');
+  }
+}
+
+class CoinMarket {
+  final String id;
+  final String name;
+  final String symbol;
+  final String price_usd;
+  final String percent_change_1h;
+  final String percent_change_24h;
+  final String percent_change_7d;
+
+  CoinMarket(
+      {this.id,
+      this.name,
+      this.symbol,
+      this.price_usd,
+      this.percent_change_1h,
+      this.percent_change_24h,
+      this.percent_change_7d});
+
+  factory CoinMarket.fromJson(Map<String, dynamic> json) {
+    return CoinMarket(
+        id: json['id'],
+        name: json['name'],
+        symbol: json['symbol'],
+        price_usd: json['price_usd'],
+        percent_change_1h: json['percent_change_1h'],
+        percent_change_24h: json['percent_change_24h'],
+        percent_change_7d: json['this.percent_change_7d']);
   }
 }
